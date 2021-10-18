cfeade29b47491d449cd20b9f677450cc6fb82e9
mayur99rv <66148617+mayur99rv@users.noreply.github.com>
2021-10-18 21:51:34 +0530

    Add files via upload

--- puppet.js
+++ puppet.js
@@ -1,4 +1,5 @@
 const fs = require("fs");
+const path = require("path");
 const puppeteer = require("puppeteer");
 
 const configJSON = fs.readFileSync("./config.json", "utf-8");
@@ -8,7 +9,13 @@ var prepend = "https://www.youtube.com";
 async function run() {
   const browser = await puppeteer.launch({
     headless: false,
-    args: ["--start-maximized"],
+    executablePath:
+      "C:\\Program Files\\Google\\Chrome\\Application\\chrome.exe",
+    args: [
+      "--start-maximized",
+      "--no-sandbox",
+      // "--disable-features=site-per-process",
+    ],
     defaultViewport: null,
   });
   let pages = await browser.pages(); // returns array of all open browser pages
@@ -16,12 +23,15 @@ async function run() {
   await page.goto(configFile.url, {
     waitUntil: "domcontentloaded",
   });
-
+  const title = await page.title();
+  console.log(title);
+  // let downPath = path.resolve("E:", title);
   let hrefs = await page.evaluate(() =>
     Array.from(document.querySelectorAll("a#video-title[href]"), (a) => {
       return a.getAttribute("href");
     })
   );
+
   let newhrefs = hrefs.map((href) => prepend.concat(href));
   let obj = {
     downloader: configFile.downloader,
@@ -29,29 +39,43 @@ async function run() {
     url: configFile.url,
   };
   //   console.log(newhrefs);
-  fs.writeFileSync("./config.json", JSON.stringify(obj), "utf-8");
+  fs.writeFileSync("./config2.json", JSON.stringify(obj), "utf-8");
   for (let i = 0; i < newhrefs.length; i++) {
-    let newPage = await browser.newPage();
-    await newPage.goto(configFile.downloader, {
-      waitUntil: "domcontentloaded",
-    });
-    //   await newPage.waitForNavigation();
-    await newPage.waitForTimeout(4000);
-    await newPage.waitForSelector("input[name='sf_url']");
-    await newPage.type("input[name='sf_url']", newhrefs[i]);
-    await newPage.waitForSelector("button#sf_submit");
-    await newPage.click("button#sf_submit");
-    await newPage.waitForTimeout(4000);
-    await newPage.waitForSelector("a.link-download");
-    await newPage.click("a.link-download");
-    let length = await (await browser.pages()).length;
-    let npages = await browser.pages();
-    console.log(length);
-    if (length === 3) {
-      await npages[2].close();
-      await npages[1].close();
-    } else if (length === 2) {
-      await npages[1].close();
+    try {
+      let newPage = await browser.newPage();
+      await newPage.goto(configFile.downloader, {
+        waitUntil: "domcontentloaded",
+      });
+      //   await newPage.waitForNavigation();
+      await newPage.waitForTimeout(4000);
+      await newPage.waitForSelector("input[name='sf_url']");
+      await newPage.type("input[name='sf_url']", newhrefs[i]);
+
+      await newPage.waitForSelector("button#sf_submit");
+      await newPage.click("button#sf_submit");
+
+      await newPage.waitForTimeout(4000);
+      await newPage._client.send("Page.setDownloadBehavior", {
+        behavior: "allow",
+        downloadPath: path.resolve("E:", "playlist_downloader", title),
+      });
+
+      await newPage.waitForTimeout(1000);
+      await newPage.waitForSelector("a.link-download");
+      await newPage.click("a.link-download");
+      await newPage.waitForTimeout(1000);
+
+      let length = await (await browser.pages()).length;
+      let npages = await browser.pages();
+      console.log(length);
+      if (length > 2) {
+        for (let k = length - 1; k > 0; k--) await npages[k].close();
+      } else if (length === 2) {
+        await npages[1].close();
+      }
+    } catch (e) {
+      console.log(e);
+      i = i - 1;
     }
   }
   //   await newPage.close();
